a3cd5f4298abe9e33fbecd3017a4389e2d20e1b8
Bryan Nix <bwyennicks@gmail.com>
2019-10-08 21:12:14 -0400

    Created Classes folder Created User.cs Declared attributes and constructor

---
+++ PizzaWebApplication/Classes/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class User
+{
+    public int id = 0;
+    public string name = string.Empty;
+    public string email = string.Empty;
+
+
+    public User(int id, string name, string email)
+    {
+        id = id;
+        name = name;
+        email = email;
+    }
+}
